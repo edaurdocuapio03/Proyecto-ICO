defcd83f5a4918960def72c1c54dd930e2254e4e
eduardocuapio03 <lalodcuapio@gmail.com>
2022-11-30 20:54:00 -0600

    Desarrollo 2

--- proyectofinal/proyectofinal/proyectofinal.cpp
+++ proyectofinal/proyectofinal/proyectofinal.cpp
@@ -7,4 +7,13 @@ int main()
     scanf_s("%i %i %i", &dia, &mes, &anio);
 
     if ((anio % 4 == 0) && (anio % 100 != 0 || anio % 400 == 0)) {
+        if (mes == 2) {
+            if (dia != 29) {
+                dia++;
+            }
+            else {
+                mes++;
+                dia = 1;
+            }
+        }
 }
